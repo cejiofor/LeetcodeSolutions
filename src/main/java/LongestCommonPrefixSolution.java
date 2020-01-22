bc2dd8db1332e55adb57f67e8739dc3328836407
ChrisIntelliJ <cejiofor@gmail.com>
2020-01-22 15:12:59 -0600

    Create LongestCommonPrefixSolution

---
+++ src/main/java/LongestCommonPrefixSolution.java
@@ -0,0 +1,11 @@
+package main.java;
+
+public class LongestCommonPrefixSolution {
+    public static String longestCommonPrefix(String[] strs) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}

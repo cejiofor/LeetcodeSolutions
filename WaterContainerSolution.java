6ec9526b52fa5471b04e2b183198e2f49b9e5db6
ChrisIntelliJ <cejiofor@gmail.com>
2020-07-09 21:02:30 -0500

    Update Water Container O(n2) and O(n) Solution

--- WaterContainerSolution.java
+++ WaterContainerSolution.java
@@ -1,25 +1,60 @@
 package com.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class WaterContainerSolution {
     public int maxArea(int[] height) {
+        int h1 = 0, h2 = height.length - 1, area = 0;
+        int maxArea = Math.abs(h2 - h1) * Math.min(height[h2], height[h1]);
+
+        while (h1 < h2) {
+            // Increment h1, check to see if new area is greater than current max area (width shrinks, but min height may increase)
+            if (height[h1] < height[h2]) {
+                h1++;
+            } else {
+                // Move in from h2 to see if the height(h2 -1) would give a larger area
+                h2--;
+            }
+            // find area with a smaller width, but potenially large min height, update maxArea if necessary
+            area = (h2 - h1) * Math.min(height[h2], height[h1]);
+            maxArea = Math.max(area, maxArea);
+        }
+        return maxArea;
+    }
+
+    // O(n^2) solution, inefficient
+    public int maxAreaDraft(int[] height) {
+        int h1 = 0, h2 = height.length-1;
+        int maxArea = Math.abs(h2 - h1) * Math.min(height[h2], height[h1]);
+
+        while (h1 < height.length-1) {
+            int areaOne = 0, areaTwo = 0;
+            for (int i = h2; i > h1; i--) {
+                int area = Math.abs(i - h1) * Math.min(height[i], height[h1]);
+                maxArea = Math.max(area, maxArea);
+            }
+            h1++;
+        }
+        return maxArea;
+    }
+
+    public int maxAreaDraftTwo(int[] height) {
         int maxAreaOne = 0, maxAreaTwo = 0;
-        int h1 = 0, h2 = 0;
-        Map<Integer, Integer> coord = new HashMap<>();
+        int h1 = 0, h2 = height.length-1;
+        int maxArea = Math.abs(h2 - h1) * Math.min(height[h2], height[h1]);
+
         int areaRight = 0, areaLeft = 0, indexArea = 0;
 
         for (int i=0; i < height.length; i++) {
-            areaLeft = height[i] * (i+1 - 0);
-            areaRight = height[i] * ((height.length+1)-i);
+            areaLeft = height[i] * (i);
+            areaRight = height[i] * (height.length-1-i);
             indexArea = Math.max(areaLeft, areaRight);
             if (indexArea > maxAreaOne){
                 maxAreaOne = indexArea;
                 h1 = i;
-            } else if (indexArea > maxAreaTwo) {
+                System.out.println(i + " ll");
+            } else if (indexArea >= maxAreaTwo && i>h2) {
                 maxAreaTwo = indexArea;
                 h2= i;
+                System.out.println(i+" kk");
             }
         }
         return Math.abs(h2 - h1) * Math.min(height[h2], height[h1]);
@@ -27,7 +62,8 @@ public class WaterContainerSolution {
 
     public static void main(String[] args) {
         WaterContainerSolution solution = new WaterContainerSolution();
-        solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+//        solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+//        solution.maxArea(new int[]{1,2,4,3});
     }
 
 }

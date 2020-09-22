d077a0f1355b7c2d014d6b30151156897aba10a9
ChrisIntelliJ <cejiofor@gmail.com>
2020-09-22 15:58:38 -0500

    Complete 3Sum solution and edit 2Sum solution

--- ThreeSumSolution.java
+++ ThreeSumSolution.java
@@ -1,7 +1,6 @@
 package com.leetcode;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class ThreeSumSolution {
     /*
@@ -9,65 +8,53 @@ public class ThreeSumSolution {
         such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
      */
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> slnSet = new ArrayList<List<Integer>>();
-        Map<Integer, Integer> map = new HashMap<>();
+        // Create a set to hold unique triplicates
+        Set<List<Integer>> slnSet = new HashSet<>();
 
-        // Add each number and a counter for each number
-        for (int num : nums) {
-            if (map.get(num) == null) {
-                map.put(num, 1);
-            } else {
-                map.put(num, map.get(num) + 1);
-            }
+        // Edge cases
+        if (nums.length < 3) {
+            return new ArrayList<List<Integer>>();
         }
 
-        for (Integer key : map.keySet()) {
-            int a=Integer.MIN_VALUE, b=Integer.MIN_VALUE, c=Integer.MIN_VALUE;
-            if (map.keySet().contains(key*-1)) {
-                if (map.keySet().contains(0)) {
-                    a = key;
-                    b = key*-1;
-                    c = 0;
-
-                    map.put(a, map.get(a) - 1);
-                    map.put(b, map.get(b) - 1);
-                    map.put(c, map.get(c) - 1);
-                    int[] sln = new int[]{a, b, c};
-                    System.out.println(Arrays.toString(sln));
-                    slnSet.add(Arrays.stream(sln).boxed().collect(Collectors.toList()));
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length-2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    List<Integer> newList = new ArrayList<>();
+                    newList.add(nums[i]);
+                    newList.add(nums[j]);
+                    newList.add(nums[k]);
+                    slnSet.add(newList);
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else if (sum > 0) {
+                    k--;
                 }
-                else{}
-            }
-
-            //Remove if count is zero
-            if (map.get(a) == 0) {
-                map.remove(a);
-            }
-            if (map.get(b) == 0) {
-                map.remove(b);
-            }if (map.get(c) == 0) {
-                map.remove(c);
             }
         }
-        return null;
 
+        return new ArrayList<List<Integer>>(slnSet);
+
+        /*
+            List<Integer> numList = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int j = i + 1; j< nums.length-1; j++) {
+                int remain = 0 - (numList.get(i) + numList.get(j));
+                if (numList.subList(j + 1, numList.size()).contains(remain)) {
+                    List<Integer> newList = new ArrayList<>();
+                    newList.add(numList.get(i));
+                    newList.add(numList.get(j));
+                    newList.add(remain);
+                    slnSet.add(newList);
+                }
+            }
+        */
     }
-
-//    public int[] remover(int num, Map map) {
-//        if (map.keySet().contains(num*-1)) {
-//            if (map.keySet().contains(0)) {
-//                a = num;
-//                b = num*-1;
-//                c = 0;
-//
-//                map.put(a, map.get(a) - 1);
-//                map.put(b, map.get(b) - 1);
-//                map.put(c, map.get(c) - 1);
-//                int[] sln = new int[]{a, b, c};
-//                System.out.println(Arrays.toString(sln));
-//                slnSet.add(Arrays.stream(sln).boxed().collect(Collectors.toList()));
-//            }
-//    }
 
     public static void main(String[] args) {
         new ThreeSumSolution().threeSum(new int[]{-1, 0, 1, 2, -1, -4});
--- TwoSumSolution.java
+++ TwoSumSolution.java
@@ -8,23 +8,20 @@ class TwoSumSolution {
     public static int[] twoSum(int[] nums, int target) {
         // List<Integer> numsList = Arrays.asList(nums);
         List<Integer> numsList = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        for (Integer num: numsList){
+        for (Integer num: numsList) {
             int indexOne = numsList.indexOf(num);
             int newTarget = target - (int) num;
             numsList.set(indexOne, null);
             int indexTwo = numsList.indexOf(newTarget);
-            if (indexTwo != -1){
-                System.out.println(indexOne + ":"+indexTwo );
+            if (indexTwo != -1) {
+                System.out.println(indexOne + ":" + indexTwo);
                 return new int[]{indexOne, indexTwo};
             }
-
         }
-        
         return new int[]{-1, -1};
     }
 
     public static void main(String[] args) {
         System.out.println(twoSum(new int[]{3,2,4}, 6));
-
     }
 }

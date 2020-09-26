aff16339f6465e2db243895207b6c7361b8611c2
ChrisIntelliJ <cejiofor@gmail.com>
2020-09-25 19:58:50 -0500

    Complete Nth node removal solution

--- ListNode.java
+++ ListNode.java
@@ -11,7 +11,9 @@ package com.leetcode;
 public class ListNode{
     int val;
     ListNode next;
+    ListNode() {}
     ListNode(int x){
         val = x;
     }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next;}
 }
---
+++ NthNodeRemoval.java
@@ -0,0 +1,49 @@
+package com.leetcode;
+
+import java.util.LinkedList;
+
+public class NthNodeRemoval {
+    /*
+        Given a linked list, remove the n-th node from the end of list and return its head.
+        Example:
+        Given linked list: 1->2->3->4->5, and n = 2
+        After removing the second node from the end, the linked list becomes 1->2->3->5.
+     */
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int count = 1;
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        while (head != null) {
+            linkedList.push(head.val);
+            head = head.next;
+        }
+
+        ListNode prevNode = null;
+        ListNode currNode = null;
+        System.out.println(linkedList.size());
+        while (!linkedList.isEmpty()) {
+            if (count == n) {
+                linkedList.pop();
+            }else{
+                currNode = new ListNode((int) linkedList.pop(), prevNode);
+                prevNode = currNode;
+            }
+            count++;
+        }
+
+        return currNode;
+    }
+
+
+}
